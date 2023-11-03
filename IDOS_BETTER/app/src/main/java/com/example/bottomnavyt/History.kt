2c9fc8780c240eca4e8d456881b36ea86a9caa68
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2023-11-03 13:58:43 +0100

    histoire open done

--- IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/History.kt
+++ IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/History.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
 
-class Finance : Fragment() {
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [History.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class History : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -23,35 +33,23 @@ class Finance : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_finance, container, false)
-
-        val buttonHistorie = view.findViewById<RelativeLayout>(R.id.buttonHistorie)
-        buttonHistorie.setOnClickListener {
-            openHistorieFragment()
-        }
-
-
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false)
     }
-
-    fun openHistorieFragment() {
-        val historieFragment = History.newInstance("param1", "param2")
-        val fragmentManager = requireActivity().supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, historieFragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
-    }
-
 
     companion object {
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
-
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment History.
+         */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Finance().apply {
+            History().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
