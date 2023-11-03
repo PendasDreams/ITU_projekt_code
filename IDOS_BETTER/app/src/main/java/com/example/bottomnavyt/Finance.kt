e2ec743c90135c63c156b014ba0a785ae677b11d
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2023-11-03 14:22:47 +0100

    Finance přidaná lišta

--- IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/Finance.kt
+++ IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/Finance.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
+import com.example.bottomnavyt.History
+
 
 class Finance : Fragment() {
     private var param1: String? = null
