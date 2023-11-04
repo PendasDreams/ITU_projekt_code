e4dcd825aa475dc76c95a4dd65afcff79ccbabc8
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2023-11-04 08:48:53 +0100

    Napojení na BE History

--- IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/DatabaseHandler.kt
+++ IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/DatabaseHandler.kt
@@ -1,9 +1,9 @@
-package com.example.bottomnavyt
-
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.content.ContentValues
+import com.example.bottomnavyt.History_obj
+
 
 
 val DATABASE_NAME = "MyDB"
@@ -16,23 +16,14 @@ val COL_CENA = "Cena"
 
 class DataBaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTable = "CREATE TABLE $TABLE_NAME (" +
-                "$COL_CAS_OD DATETIME," +
-                "$COL_CAS_DO DATETIME," +
-                "$COL_MISTO_OD VARCHAR(255)," +
-                "$COL_MISTO_DO VARCHAR(255)," +
-                "$COL_CENA DECIMAL(10,2))"
+        val createTable =
+            "CREATE TABLE $TABLE_NAME (" +
+                    "$COL_CAS_OD DATETIME," +
+                    "$COL_CAS_DO DATETIME," +
+                    "$COL_MISTO_OD VARCHAR(255)," +
+                    "$COL_MISTO_DO VARCHAR(255)," +
+                    "$COL_CENA DECIMAL(10,2))"
         db?.execSQL(createTable)
-
-        // Vložení výchozího záznamu při vytváření tabulky
-        val initialValues = ContentValues()
-        initialValues.put(COL_CAS_OD, "2023-11-01 08:00:00")
-        initialValues.put(COL_CAS_DO, "2023-11-01 12:00:00")
-        initialValues.put(COL_MISTO_OD, "Místo1")
-        initialValues.put(COL_MISTO_DO, "Místo2")
-        initialValues.put(COL_CENA, 100.0)
-
-        db?.insert(TABLE_NAME, null, initialValues)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -54,5 +45,22 @@ class DataBaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         contentValues.put(COL_CENA, 100.00) // Příklad ceny
 
         db?.insert(TABLE_NAME, null, contentValues)
+    }
+
+    // Metoda pro vkládání dat do databáze
+    fun insertData(data: History_obj): Long {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+
+        contentValues.put(COL_CAS_OD, data.casOd?.toString()) // Předpokládáme, že casOd je String
+        contentValues.put(COL_CAS_DO, data.casDo?.toString()) // Předpokládáme, že casDo je String
+        contentValues.put(COL_MISTO_OD, data.mistoOd)
+        contentValues.put(COL_MISTO_DO, data.mistoDo)
+        contentValues.put(COL_CENA, data.cena.toDouble()) // Převést cenu na Double
+
+        val insertedId = db.insert(TABLE_NAME, null, contentValues)
+        db.close()
+
+        return insertedId
     }
 }
--- IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/History.kt
+++ IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/History.kt
@@ -1,79 +1,114 @@
 package com.example.bottomnavyt
 
-
+import DataBaseHandler
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import java.math.BigDecimal
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import java.util.Date
-
+import com.example.bottomnavyt.History_obj
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [History.newInstance] factory method to
- * create an instance of this fragment.
- */
+class History_obj(
+    val casOd: String?,
+    val casDo: String?,
+    val mistoOd: String?,
+    val mistoDo: String?,
+    val cena: Int
+)
 
-class History_obj {
-    var casOd: Date? = null
-    var casDo: Date? = null
-    var mistoOd: String = ""
-    var mistoDo: String = ""
-    var cena: Int = 0
-
-    constructor(casOd: Date?, casDo: Date?, mistoOd: String, mistoDo: String, cena: Int) {
-        this.casOd = casOd
-        this.casDo = casDo
-        this.mistoOd = mistoOd
-        this.mistoDo = mistoDo
-        this.cena = cena
-    }
-}
 class History : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Declare UI elements
+    private lateinit var editTextCasOd: EditText
+    private lateinit var editTextCasDo: EditText
+    private lateinit var editTextMistoOd: EditText
+    private lateinit var editTextMistoDo: EditText
+    private lateinit var editTextCena: EditText
+    private lateinit var buttonPridat: Button
+    private lateinit var textViewResult: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
+
+        // Initialize UI elements
+        editTextCasOd = view.findViewById(R.id.editTextCasOd)
+        editTextCasDo = view.findViewById(R.id.editTextCasDo)
+        editTextMistoOd = view.findViewById(R.id.editTextMistoOd)
+        editTextMistoDo = view.findViewById(R.id.editTextMistoDo)
+        editTextCena = view.findViewById(R.id.editTextCena)
+        buttonPridat = view.findViewById(R.id.buttonPridat)
+        textViewResult = view.findViewById(R.id.textViewResult)
+
+        // Set click listener for the "Přidat" button
+        buttonPridat.setOnClickListener {
+            try {
+                val data = getFormData() // Získání hodnot z formuláře
+                val dbHandler = DataBaseHandler(requireContext()) // Vytvoření instance DataBaseHandler
+
+                // Vložení dat do databáze
+                val insertedId = dbHandler.insertData(data)
+
+                // Získání aktuálního textu z TextView
+                val currentText = textViewResult.text.toString()
+
+                // Vytvoření řetězce obsahujícího data z objektu History_obj
+                val newData = "Cas od: ${data.casOd}, Cas do: ${data.casDo}, Misto od: ${data.mistoOd}, Misto do: ${data.mistoDo}, Cena: ${data.cena}"
+
+                // Vytvoření nového textu, který zahrnuje předchozí data a nová data
+                val newText = "$currentText\nData byla úspěšně vložena s ID: $insertedId\n$newData"
+
+                // Aktualizace textu v TextView
+                textViewResult.text = newText
+            } catch (e: Exception) {
+                val errorMessage = "Chyba při vkládání dat: ${e.message}"
+                textViewResult.text = errorMessage
+            }
+        }
+
+        return view
+    }
+    private fun getFormData(): History_obj {
+        val casOd = editTextCasOd.text.toString()
+        val casDo = editTextCasDo.text.toString()
+        val mistoOd = editTextMistoOd.text.toString()
+        val mistoDo = editTextMistoDo.text.toString()
+        val cena = editTextCena.text.toString().toInt()
+
+        return History_obj(casOd, casDo, mistoOd, mistoDo, cena)
+    }
+
+    private fun insertDataToDatabase() {
+        val data = getFormData()
+        val dbHandler = DataBaseHandler(requireContext())
+
+        val insertedId = dbHandler.insertData(data)
+
+        val message = "Data byla úspěšně vložena s ID: $insertedId"
+        textViewResult.text = message
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment History.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            History().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String, param2: String): History {
+            val fragment = History()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
