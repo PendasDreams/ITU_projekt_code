2e1cab52776ec174e6ea7582a16fd386384e870e
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2023-11-05 16:52:02 +0100

    kliknuti na button minecraft

--- IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/Home.kt
+++ IDOS_BETTER/app/src/main/java/com/example/bottomnavyt/Home.kt
@@ -5,19 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.ImageView
+import com.example.bottomnavyt.History
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,23 +26,60 @@ class Home : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_finance, container, false)
+
+        val buttonHistorie = view.findViewById<RelativeLayout>(R.id.buttonHistorie)
+        buttonHistorie.setOnClickListener {
+            openHistorieFragment()
+        }
+
+        val buttonPrehledVydaju = view.findViewById<RelativeLayout>(R.id.buttonPrehledVydaju)
+        buttonPrehledVydaju.setOnClickListener {
+            openPrehledVydajuFragment()
+        }
+
+        val buttonAddEntry = view.findViewById<ImageView>(R.id.addNewEntry)
+        buttonAddEntry.setOnClickListener {
+            openAddCreditFragment()
+        }
+
+
+        return view
+    }
+
+    fun openHistorieFragment() {
+        val spojeniFragment = Spojeni.newInstance("param1", "param2")
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,spojeniFragment )
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    fun openPrehledVydajuFragment() {
+        val expensesOverviewFragment = ExpensesOverview.newInstance("param1", "param2")
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, expensesOverviewFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun openAddCreditFragment() {
+        val addCreditFragment = AddCredit.newInstance() // Replace with the actual new fragment class
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, addCreditFragment) // Use your container id
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Home().apply {
+            Finance().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
